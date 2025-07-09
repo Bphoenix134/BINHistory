fa1454dcfab24878a807ab2cebd18b4c923bf9db
Kris <blackphoenix134@mail.ru>
2025-07-10 01:48:21 +0300

    feat(ui): update MainActivity with theme and nav Set MaterialTheme in MainActivity setContent. Add NavController for navigation setup. Include NavGraph for routing in Compose.

--- app/src/main/java/com/example/binhistory/presentaion/MainActivity.kt
+++ app/src/main/java/com/example/binhistory/presentaion/MainActivity.kt
@@ -3,6 +3,9 @@ package com.example.binhistory.presentaion
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.material3.*
+import androidx.navigation.compose.rememberNavController
+import com.example.binhistory.presentaion.navigation.NavGraph
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -10,6 +13,10 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
+            MaterialTheme {
+                val navController = rememberNavController()
+                NavGraph(navController = navController)
+            }
         }
     }
 }
