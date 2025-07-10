658e068131ed5d7b20c6bd9ade0d57c970684ad2
Kris <blackphoenix134@mail.ru>
2025-07-10 12:02:51 +0300

    feat(ui): add navigation to CardHistoryScreen Add navigation button to CardInfoScreen. Pass NavController to enable navigation to card_history route. Update NavGraph to handle routes.

--- app/src/main/java/com/example/binhistory/presentation/navigation/NavGraph.kt
+++ app/src/main/java/com/example/binhistory/presentation/navigation/NavGraph.kt
@@ -1,17 +1,17 @@
-package com.example.binhistory.presentaion.navigation
+package com.example.binhistory.presentation.navigation
 
 import androidx.compose.runtime.Composable
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
-import com.example.binhistory.presentaion.cardInfo.CardInfoScreen
-import com.example.binhistory.presentaion.cardHistory.CardHistoryScreen
+import com.example.binhistory.presentation.cardInfo.CardInfoScreen
+import com.example.binhistory.presentation.cardHistory.CardHistoryScreen
 
 @Composable
 fun NavGraph(navController: NavHostController) {
     NavHost(navController = navController, startDestination = "card_info") {
         composable("card_info") {
-            CardInfoScreen()
+            CardInfoScreen(navController = navController)
         }
         composable("card_history") {
             CardHistoryScreen()
